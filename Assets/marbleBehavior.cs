f1099ed07a550126028027cfed772f8b69bba145
Luke Roberts <luke.r35@googlemail.com>
2016-02-26 14:29:47 +0000

    Adds Automatic Marble Spawning and Real/Fake System

---
+++ Assets/marbleBehavior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class marbleBehavior : MonoBehaviour {
+
+    public bool isFake = false;
+    public Sprite real;
+    public Sprite fake;
+
+    void Start ()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        if (isFake == false)
+        {
+            sr.sprite = real;
+        }
+        else
+        {
+            sr.sprite = fake;
+        }
+    }
+
+    public void catchMarble ()
+    {
+            if (isFake == false)
+            {
+                Debug.Log("Real Marble was Caught!");
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Fake Marble was Caught!");
+                Destroy(gameObject);
+            }
+
+
+    }
+}
---
+++ Assets/marbleController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class marbleController : MonoBehaviour {
+    public int x;
+    public int percentOfFake;
+    public GameObject Marble1;
+    private GameObject[] noOfMarbles;
+    private bool hasRun = false;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(createInitialMarbles());
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator createInitialMarbles ()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            yield return new WaitForSeconds(1);
+            int rand = Random.Range(0, 100);
+
+            if (rand > percentOfFake)
+            {
+                Marble1.GetComponent<marbleBehavior>().isFake = false;
+            }
+            else
+            {
+                Marble1.GetComponent<marbleBehavior>().isFake = true;
+            }
+
+            Instantiate(Marble1, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+
+
+
+
+
+}
