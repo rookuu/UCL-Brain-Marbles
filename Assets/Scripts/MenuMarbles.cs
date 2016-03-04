33b54cb065c443a5cdb3f8b575d535d8f992d6c3
Luke Roberts <luke.r35@googlemail.com>
2016-03-04 16:49:17 +0000

    UI Foundations and Asset Restructure

---
+++ Assets/Scripts/MenuMarbles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMarbles : MonoBehaviour {
+
+	int numberOfPathNodes;
+	public int speedOfMarble = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		numberOfPathNodes = 1000;
+		Vector3[] path = new Vector3[numberOfPathNodes];
+
+		for (int i = 0; i < numberOfPathNodes; i++)
+		{
+			path[i] = new Vector3(Random.Range(-10, 10), Random.Range(-6, 6), 0);
+		}
+
+		runThroughPath(path);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void runThroughPath(Vector3[] path)
+	{
+		iTween.MoveTo(gameObject, iTween.Hash("path", path, "speed", speedOfMarble, "easetype",  iTween.EaseType.linear, "oncomplete", "flyOffScreen"));
+	}
+		
+
+
+}
