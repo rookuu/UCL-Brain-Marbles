2e745f2463ac1652dd1d6c4f416379031efeebdf
Luke Roberts <luke.r35@googlemail.com>
2016-03-15 10:55:31 +0000

    Adds Basic Persistence
    Persistent GameObject and LoggedIn / UserEmail

---
+++ Assets/Scripts/globalData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class globalData : MonoBehaviour {
+
+	public static globalData instance;
+	public bool loggedIn = false;
+	public string userEmail = null;
+
+	void Start () {
+		loggedIn = globalData.instance.loggedIn;
+		userEmail = globalData.instance.userEmail;
+	}
+	
+	// Update is called once per frame
+	void Awake () {
+		if (instance == null) {
+			DontDestroyOnLoad (gameObject);
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void saveData()
+	{
+		globalData.instance.loggedIn = loggedIn;
+		globalData.instance.userEmail = userEmail;
+	}
+}
---
+++ Assets/Scripts/login.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class login : MonoBehaviour {
+	public Text email;
+	public Text password;
+
+	public void loginUser () {
+		//Validation(email);
+		//Validation(password);
+
+		//Authenticate(email, password);
+
+		globalData data = GameObject.Find ("GlobalData").GetComponent<globalData> ();
+		data.loggedIn = true;
+		data.userEmail = email.text;
+		data.saveData ();
+		SceneManager.LoadScene (5);
+
+	}
+}
